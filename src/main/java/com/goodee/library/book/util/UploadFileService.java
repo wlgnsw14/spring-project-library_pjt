3d9d95e3cf55ae662046f88184a62933fadd37ed
wlgnsw14 <wlgnsw14@naver.com>
2024-04-12 17:52:02 +0900

    edit : file path

--- src/main/java/com/goodee/library/book/util/UploadFileService.java
+++ src/main/java/com/goodee/library/book/util/UploadFileService.java
@@ -11,52 +11,52 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class UploadFileService {
+   private static final Logger LOGGER = LogManager.getLogger(UploadFileService.class);
+   
+   private String uploadDir = "/var/lib/tomcat9/webapps/upload/";
 
-	private static final Logger LOGGER =
-			LogManager.getLogger(UploadFileService.class);
-	
-	private String uploadDir = "/var/lib/tomcat9/webapps/upload/";
-	
-	public String upload(MultipartFile file) {
-		LOGGER.info("파일을 서버에 저장");
-		
-		boolean result = false; // 판단의 기준점
-		
-		String origin = file.getOriginalFilename();
-		String extension = origin.substring(origin.lastIndexOf("."),origin.length());
-		UUID uuid = UUID.randomUUID();
-		String change = uuid.toString().replaceAll("-", "");
-		
-		File savedFile = new File(uploadDir+change+extension);
-		
-		if(savedFile.exists() == false) {
-			savedFile.mkdir();
-		}
-		try {
-			file.transferTo(savedFile);
-			result = true;
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		if(result == true) {
-			return change+extension;
-		}else {
-			return null;
-		}
-	}
-	
-	public boolean delete(String b_thumbnail) {
-		boolean result = false;
-		try {
-		   String srcFileName = URLDecoder.decode(b_thumbnail,"UTF-8");
-		   File file = new File(uploadDir+srcFileName);
-		   if(file.exists()) {
-		      result = file.delete();
-		   }   
-		}catch(Exception e) {
-		   e.printStackTrace();
-		}
-		return result;
-	}
+   public String upload(MultipartFile file) {
+      LOGGER.info("파일 서버에 등록");
+      boolean result = false;
+      String fileOriName = file.getOriginalFilename();
+      String fileExtension =
+            fileOriName.substring(fileOriName.lastIndexOf("."),fileOriName.length());
+      
+      UUID uuid = UUID.randomUUID();
+      String uniqueName = uuid.toString().replaceAll("-", "");
+      
+      File saveFile = new File(uploadDir+uniqueName+fileExtension);
+      
+      if(!saveFile.exists())
+         saveFile.mkdirs();
+      
+      try {
+         file.transferTo(saveFile);
+         result = true;
+      } catch(Exception e) {
+         LOGGER.error(e.toString());
+      }
+      
+      if(result) {
+         LOGGER.info("파일 업로드 성공!!");
+         return uniqueName+fileExtension;
+      } else {
+         LOGGER.info("파일 업로드 실패ㅠㅠ");
+      }
+      return null;
+   }
+   
+      public boolean delete(String b_thumbnail) {
+            boolean result = false;
+            try {
+               String srcFileName = URLDecoder.decode(b_thumbnail,"UTF-8");
+               File file = new File(uploadDir+srcFileName);
+               if(file.exists()) {
+                  result = file.delete();
+               }   
+            }catch(Exception e) {
+               e.printStackTrace();
+            }
+            return result;
+         }
 }
